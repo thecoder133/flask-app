875da12c42d25e64362921652a0e41352c91df07
thecoder133 <64936766+thecoder133@users.noreply.github.com>
2026-01-13 14:18:51 -0800

    lkjhlkhlkjhlkjhlk

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
-@app.route('/num/<number>')
+@app.route('/num/<int:number>')
 def num(number):
     num = random.randint(1, number)
     return f"Number between 1 and {number} is: {num}!"
