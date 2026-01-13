d6dc8e0142bbaad3016726c8fe8b1bc0bd488ba4
thecoder133 <64936766+thecoder133@users.noreply.github.com>
2026-01-13 11:46:40 -0800

    Update app.pyyes

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
+import random
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    num = random.randint(0, 99)
+    return render_template('index.html', num=num)
